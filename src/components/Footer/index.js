4d1cdfde8ac6a36873bda5acb6942ee95acd6e69
angel208 <angel_fp90@hotmail.com>
2020-11-18 20:50:52 -0300

    footer's arrow fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,7 +12,7 @@ export default function Footer() {
     <div
       className={`bg-secondary-background pb-12 pt-20 h-auto w-full flex flex-col justify-center items-center relative`}
     >
-      <button className="w-8 h-20 bg-primary absolute -mt-8 flex justify-center items-center transform ease-in  duration-200 hover:scale-105">
+      <button className="w-8 h-20 top-0 bg-primary absolute -mt-8 flex justify-center items-center">
         <FaArrowUp className="text-main-text font-light h-full" />
       </button>
       <h1 className="px-6 py-6 text-2xl lg:text-3xl md:w-5/10 lg:w-3/10 text-center leading-tight ">
