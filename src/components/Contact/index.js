4886f55da24cdc0fa851d0450dba8b5802effb6d
angel208 <angel_fp90@hotmail.com>
2020-11-20 02:18:55 -0300

    contact pt1

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,7 +10,24 @@ import {
 export default function Contact() {
   return (
     <div
-      className={`background-secondary-background h-screen w-full flex justify-center items-center object-fill`}
-    ></div>
+      className={` h-auto w-full px-32 py-24 lg:py-32 xl:px-48 flex flex-col-reverse lg:flex-row`}
+    >
+      <div name="form" className="w-full lg:w-2/3 bg-secondary-background">
+        <p>sad</p>
+      </div>
+      <div
+        name="contact-text"
+        className="w-full lg:w-1/3 lg:mx-12 text-center lg:text-left py-8"
+      >
+        <p className="text-primary">Lets Talk</p>
+        <h2 className="text-2xl py-2">Contact Me</h2>
+        <p className="text-secondary-text py-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor
+          nulla in porta faucibus. Nam at convallis magna. Vivamus vitae odio
+          posuere, molestie lorem sit amet, ultricies quam. Nunc mollis mollis
+          sapien, at ornare velit pretium non. Nam vel elit odio.
+        </p>
+      </div>
+    </div>
   )
 }
